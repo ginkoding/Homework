05444b2c1be419e13fa8b019a90c37efa386875d
Rebecca Sewell <rebecca.lee.sewell@gmail.com>
2022-06-26 11:32:16 +0100

    week two

---
+++ week-two/homework.py
@@ -0,0 +1,66 @@
+# my_name = 'Penelope'
+
+# my_age = 29
+
+# message = 'My name is {} and I am {} years old'.format(my_name, my_age)
+
+# print(message)
+
+
+# for number in range(100):
+
+#     output = 'o' * number
+
+# print(output)
+
+
+# wrd = "Python"
+
+# ans_1 = slice(4, 5)
+
+# print(ans_1)
+
+
+# def calculate_vat(amount):
+
+#     amount * 1.2
+
+
+# total = calculate_vat(100)
+
+# print(total)
+
+
+# weather = input('Is it raining?')
+
+# is_raining = raining  == 'Raining'
+
+# if is_raining == y:
+# # print('It is raining:Take an umbrella')
+# else:
+# # print('It is not raining')
+
+
+# my_money = input('How much money do you have?')
+# boatcost = 20 + 5
+# if my_money < boatcost:
+#     print('You can afford the boat hire')
+# else:
+#     print('You canot afford the board hire')
+
+
+# shopping_list = [
+
+#     "oranges",
+
+#     "cat food",
+
+#     "sponge cake",
+
+#     "long-grain rice",
+
+#     "cheese board",
+
+# ]
+
+# print(shopping_list[1])
